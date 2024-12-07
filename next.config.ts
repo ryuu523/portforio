91822f1a8b007047b789da0098cc22000a38e913
ryuu523 <ryu20050523@yahoo.co.jp>
2024-12-08 03:18:07 +0900

    test

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,17 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production';
+
+// リポジトリ名に置き換えてください
+const repoName = 'portforio';
 
 const nextConfig: NextConfig = {
-
+  assetPrefix: isProd ? `/${repoName}/` : '',
+  basePath: isProd ? `/${repoName}` : '',
+  images: {
+    unoptimized: true,
+  },
+  output: 'export',
 };
 
 export default nextConfig;
