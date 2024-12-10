ca38da54a923d61e8b473564efe0108519c74bfa
ryuu523 <ryu20050523@yahoo.co.jp>
2024-12-10 17:41:15 +0900

    worldmap仮作成

--- components/block.tsx
+++ components/block.tsx
@@ -1,20 +1,38 @@
 import Image, { StaticImageData } from "next/image";
-import back1 from "../public/images/maptile_sogen_01.png"
-import back2 from "../public/images/maptile_sogen_02.png"
+import grass1 from "../public/images/mapTile/maptile_sogen_01.png"
+import grass2 from "../public/images/mapTile/maptile_sogen_02.png"
+import see1 from "../public/images/mapTile/maptile_umi_01.png"
+import see2 from "../public/images/mapTile/maptile_umi_02.png"
+import snow from "../public/images/mapTile/maptile_setsugen.png"
+import sand from "../public/images/mapTile/maptile_sabaku.png"
+import lava from "../public/images/mapTile/maptile_yogan.png"
+
+import bridge from "../public/images/bridge_side_brown.png"
+import hole from "../public/images/ana_gray.png"
+
 import "../styles/block.css"
+
 type BlockProps = {
     type: number;
 };
+
 export default function Block({ type }: BlockProps) {
     const getImageSrc = (): string => {
         switch (type) {
             case 1:
-                return back1.src;
+                return see1.src;
             case 2:
-                return back2.src;
-
+                return grass1.src;
+            case 3:
+                return snow.src;
+            case 4:
+                return sand.src;
+            case 5:
+                return lava.src;
+            case 6:
+                return bridge.src
             default:
-                return back1.src;
+                return grass2.src;
         }
     }
     return (
